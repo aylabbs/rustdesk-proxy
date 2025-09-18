45d46110acbec10b26712ee635698fb5bbfd9133
Abraham Labkovsky <aylabbs@gmail.com>
2025-09-18 12:33:37 -0400

    works

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,26 @@
 export async function handler(event, context) {
+  const apiRes = await fetch(
+    "https://api.github.com/repos/rustdesk/rustdesk/releases/latest",
+    {
+      headers: { "User-Agent": "node.js" },
+    }
+  );
+
+  if (!apiRes.ok) throw new Error(`Failed to fetch release: ${apiRes.status}`);
+
+  const release = await apiRes.json();
+
+  const asset = release.assets.find((a) =>
+    a.name.match(/^rustdesk-.*x86_64\.exe$/)
+  );
+
+  if (!asset) throw new Error("No matching Windows EXE found.");
+
+  const url = asset.browser_download_url;
   return {
     statusCode: 200,
     body: JSON.stringify({
-      url: "/pdfs/sample.pdf", // assumes you put the file in "public/pdfs/"
+      url,
     }),
     headers: {
       "Content-Type": "application/json",
